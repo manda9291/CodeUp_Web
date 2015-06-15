296a0480a8c20a113e194a70adccbcc0ed172f1a
Manda9291 <manda9291@yahoo.com>
2015-06-15 15:06:39 -0500

    first commit

---
+++ public/my_first_form.php
@@ -0,0 +1,116 @@
+<?php
+  var_dump($_GET);
+  var_dump($_POST);
+?>
+<formmethod="post" action"/my_first_form.php">
+	<p> <Label for="Email">Email</label>
+		<input type="text" id="Email" name="email" placeholder="Email">
+	<p><label for="password">Password</label>
+		<input type="password" id="Password" name="password" placeholder="Password goes here"></p>
+
+			<button type="submit">Log in Now</button>
+</form>
+
+
+<section>
+
+<form>
+	<form method="post" action="/my_first_form.php">
+  		<p><label for="to">to</label></p>
+			<input type="text" id="to" name="to" placeholder="to">
+  		<p></p><label for="from">from</label></p>
+			<input type="text" id="from" name="from" placeholder="from">
+  		<p></p><label for="subject">subject</label></p>
+			<input type="text" id="subject" name="subject" placeholder="subject">
+  		<p></p><label for="body">body</label></p>
+			<input type="text" id=to" name="body" placeholder="body">
+		<p></p><input type="submit" Value="send">
+		<p></p> <input type="checkbox" id="send_email" name="compose_an_email" value="yes" checked>
+		<label for="mailing_list">Send Email Now!</label>
+
+		
+</from
+
+</section>
+
+<section>
+
+<h2>Multiple Choice Test</h2>
+What is my Favorie Color?
+<label>
+	<input type="radio" name="Q1" value="black">
+	Black
+</label>
+<label>
+	<input type="radio" name="Q1" value="yellow">
+	Yellow
+</label>
+<label>
+	<input type="radio" name="Q1" value="green">
+	Green
+</label>
+<label>
+	<input type="radio" name="Q1" value="red">
+	Red
+</label>
+
+<p></p>What is the name of yourMr.Kitty
+<label>
+	<input type="radio" name="Q1" value="Mr.Kitty">
+	Mr.Kitty
+</label>
+<label>
+	<input type="radio" name="Q1" value="Blue">
+	Blue
+</label>
+<label>
+	<input type="radio" name="Q1" value="Smudge">
+	Smudge
+</label>
+<label>
+	<input type="radio" name="Q1" value="Chico">
+	Chico
+</label>
+<p></p><button>Send it</button>
+</section>
+<p></p>
+<h2></h2>
+
+<section> 
+	What are some of your vacation spots?
+
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="Garner State Park"> Garner State Park</laspot
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="Port Aransas">Port Aransas</label>
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="Devine"> Devine
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="Corpus Christi"> Corpus christi</label>
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="South Padre Island"> South Padre Island</label>
+<p></p><label><input type="checkbox" id="vacay" name="spot" value="Yosemite"> Yosemite</label>
+<p></p><button>Send it</button>
+
+<form>
+	<h2>Select Testing</h2>
+
+<label for="heights">Are you scared of heights: </label>
+	<select id="heights" name="heights"
+		
+		<option></option>
+		<option value="1">Maybe</option>
+		<option value="2">Yes</option>
+		<option value="3">No</option>
+	</select>
+<BUTTON>Enter</BUTTON>
+
+
+
+
+
+
+
+
+
+
+
+</form>
+
+
+
