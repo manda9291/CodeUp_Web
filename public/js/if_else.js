82ccf614e1f64234599ac157bf63fb56f70510b0
Manda9291 <manda9291@yahoo.com>
2015-06-29 11:08:40 -0500

    new

---
+++ public/js/if_else.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<switch>
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors =  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+var favorite = 'red'; // TODO: change this to your favorite color from the list
+
+console.log(color);
+
+// TODO: Create a block of if/else statements to check for every color except indigo and violet.
+
+// TODO: When a color is encountered log a message that tells the color, and an object of that color.
+//       Example: Blue is the color of the sky.
+if (color == 'red'){
+console.log('red is the color of an apple');
+}else(color == 'orange'){
+console.log('red is the color of an orange');
+else(color was not red or orange)
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+// TODO: Using the ternary operator, conditionally log a statement that
+//       says whether the random color matches your favorite color.
+</switch>
+
+</html>
